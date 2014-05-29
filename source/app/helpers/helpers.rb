a36dfc279c37677d7abe508d91f66e7b5f32d958
Dusty Wilson & Sammy Rihawi <pair+dusty-wilson.nairys@devbootcamp.com>
2014-05-29 16:54:07 -0500

    working vote creation with slider

--- source/app/helpers/helpers.rb
+++ source/app/helpers/helpers.rb
@@ -7,4 +7,10 @@ helpers do
   def current_user
     user = User.find_by_id(session[:user_id])
   end
+
+  def get_random_user_and_question(user)
+    @possible_users = (User.all-[user])
+    @sampled_user = @possible_users.sample
+    @sampled_question = Property.all.sample
+  end
 end
