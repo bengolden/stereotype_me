10b47239d1d0b4a00ff9f982e58d3509475c6b86
Ben Golden <bengolden1@gmail.com>
2014-05-29 20:20:21 -0500

    tweaked layout

--- source/app/models/vote.rb
+++ source/app/models/vote.rb
@@ -4,6 +4,6 @@ class Vote < ActiveRecord::Base
   belongs_to :voted_on, class_name: "User"
   belongs_to :property
 
-  validates_uniqueness_of :property, scope: [:voter_id, :voted_on_id]
+  # validates_uniqueness_of :property, scope: [:voter_id, :voted_on_id]
 
 end
